ee1f4941e95da1dcb9a7dc9ebfa071442411aa32
Adithya Nair <adtya@adtya.xyz>
2021-12-11 23:52:59 +0530

    add init command

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Printf("Usage: %s <command>\n", os.Args[0])
+		os.Exit(1)
+	}
+	switch args[0] {
+	case "init":
+		Init(args[1:])
+	default:
+		fmt.Printf("%s is an invalid command\n", args[0])
+		os.Exit(1)
+	}
+}
+
+func Init(args []string) {
+	fmt.Println("Hello, World!")
 }
